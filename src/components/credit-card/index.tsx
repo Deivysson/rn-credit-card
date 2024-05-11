b7c04d6c439b7a86c892c4737857806754328af8
Deivysson Gomes <105027772+Deivysson@users.noreply.github.com>
2024-05-10 22:07:41 -0300

    commit

--- src/components/credit-card/index.tsx
+++ src/components/credit-card/index.tsx
@@ -11,8 +11,34 @@ export function CreditCard(){
         </View>
 
         <View style={styles.footer}>
+            <Text style={styles.name}>Deivysson Gomes</Text>
 
+            <View style={styles.flag}>
+            <View style={[styles.circle, styles.red]} />
+            <View style={[styles.circle, styles.orange]} />
         </View>
+        </View>
+        </View>
+
+        <View style={[styles.card, styles.back]}>
+            <View>
+                <Text style={styles.label}>Numero do cartao</Text>
+                <Text style={styles.value}>1234 5678 9012 3456</Text>
+            </View>
+
+            <View style={styles.footer}>
+                <View>
+                <Text style={styles.label}>Validade</Text>
+                <Text style={styles.value}>12/02</Text>
+                </View>
+
+                <View>
+                <Text style={styles.label}>CVV</Text>
+                <Text style={styles.value}>123</Text>
+                </View>
+
+            </View>
+
         </View>
     </View>
     )
--- src/components/credit-card/styles.ts
+++ src/components/credit-card/styles.ts
@@ -1,7 +1,58 @@
 import { StyleSheet } from "react-native";
 
 export const styles = StyleSheet.create({
-    card: {},
-    front: {},
-    back: {},
+    card: {
+        width: "100%",
+        height: 200,
+        borderRadius: 12,
+        padding: 24,
+        justifyContent: 'space-between',
+    },
+
+    front: {
+        backgroundColor: '#dae1e7',
+    },
+
+    back: {
+        backgroundColor: '#bac1c7',
+    },
+
+    circle: {
+        width: 24,
+        height: 24,
+        borderRadius: 12,
+    },
+
+    logo: {
+        backgroundColor: '#8795a0',
+    },
+
+    header: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        gap: 7,
+    },
+
+    name: {
+        fontSize: 16,
+        fontWeight: 'bold',
+    },
+
+    footer: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+    },
+
+    flag: {
+        flexDirection: 'row',
+        gap: -12 ,
+    },
+
+    red: {
+        backgroundColor: 'red',
+    },
+
+    orange: {
+        backgroundColor: 'orange',
+    },
 })
